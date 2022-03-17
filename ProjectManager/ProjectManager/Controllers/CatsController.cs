7160a1b003132df8260612196d374570c01d484d
Zhekov Georgi (HAU-ITDW) <Georgi.Zhekov2@liebherr.com>
2022-03-17 17:54:20 +0200

    small changes

---
+++ ProjectManager/ProjectManager/Controllers/CatsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManager.Models;
+
+namespace ProjectManager.Controllers
+{
+    public class CatsController : Controller
+    {
+        public IActionResult All()
+        {
+            var cats = new List<CatViewModel>()
+            {
+                new CatViewModel{Name = "Kolyu", Age = 2},
+                new CatViewModel{Name = "Kotio", Age = 1}
+            };
+
+            return View(cats);
+        }
+    }
+}
+ 
---
+++ ProjectManager/ProjectManager/Models/CatViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectManager.Models
+{
+    public class CatViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
